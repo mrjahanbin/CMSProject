3f273427d60032f06f047c112a1b8e22f4014bea
Mrjahanbin <Mrjahanbin@eshareha.ir>
2020-03-26 19:23:27 +0430

    Create Interface IPageGroupRepository

---
+++ CMSProject.Services/Repositores/IPageGroupRepository.cs
@@ -0,0 +1,18 @@
+﻿using CMSProject.DomainClass.PageGroup;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMSProject.Services.Repositores
+{
+    public interface IPageGroupRepository
+    {
+        List<PageGroup> GellAppPageGroup();
+        PageGroup GetPageGroupById(int groupId);
+        int InsertPageGroup(PageGroup pageGroup);
+        void UpdatePageGroup(PageGroup pageGroup);
+        void DeletePageGroup(int GroupId);
+        void DeletePageGroup(PageGroup pageGroup);
+
+    }
+}
