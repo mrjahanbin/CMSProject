647fd2eaf8e16b65d53c2950468c80addc4bbdf3
Mrjahanbin <Mrjahanbin@eshareha.ir>
2020-03-26 20:00:28 +0430

    Add IPageRepository

---
+++ CMSProject.Services/Repositores/IPageRepository.cs
@@ -0,0 +1,21 @@
+﻿using CMSProject.DomainClass.Page;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMSProject.Services.Repositores
+{
+    public interface IPageRepository
+    {
+        IEnumerable<Page> GetAllPage();
+
+        Page GetPageById(int pageId);
+
+        void InsertPage(Page page);
+        void UpdatePage(Page page);
+        void DeletePage(Page page);
+        void DeletePage(int pageId);
+        void Save();
+
+    }
+}
